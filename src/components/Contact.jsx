edb02df5db2e27c8e69c80b282391f5c181b96c5
Adham Hatem <191885145+Adham-Hatem@users.noreply.github.com>
2026-05-20 11:49:07 +0300

    test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,41 +8,31 @@ import "../main.css";
 function Contact() {
   return (
     <section id="contact" className="contactSection">
-      <div className="column">
-        <div className="contactContainer">
-          <h1>Contact Me</h1>
-          <p>
-            Feel free to reach out to me through any of the following platforms:
-          </p>
+      <div className="contactContainer">
+        <h1>Contact Me</h1>
+        <p>
+          Feel free to reach out to me through any of the following platforms:
+        </p>
 
-          <div className="iconRow">
-            <a
-              href="https://mail.google.com/mail/?view=cm&fs=1&to=adhamhatemgomaa@gmail.com"
-              target="_blank"
-            >
-              <img src={mailIcon} alt="Email" className="icon" />
-            </a>
-            <a href="https://wa.me/+201028675020" target="_blank">
-              <img src={whatsappIcon} alt="WhatsApp" className="icon" />
-            </a>
-            <a
-              href="https://linkedin.com/in/adham-hatem-94bb99343/"
-              target="_blank"
-            >
-              <img src={linkedinIcon} alt="LinkedIn" className="icon" />
-            </a>
-          </div>
-        </div>
-
-        <div className="contactContainer">
-          <h1>Socials</h1>
-          <p>Follow my Platforms to stay updated with my latest projects!</p>
-
-          <div className="iconRow">
-            <a href="https://github.com/Adham-Hatem" target="_blank">
-              <img src={githubIcon} alt="GitHub" className="icon" />
-            </a>
-          </div>
+        <div className="iconRow">
+          <a
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=adhamhatemgomaa@gmail.com"
+            target="_blank"
+          >
+            <img src={mailIcon} alt="Email" className="icon" />
+          </a>
+          <a href="https://wa.me/+201028675020" target="_blank">
+            <img src={whatsappIcon} alt="WhatsApp" className="icon" />
+          </a>
+          <a
+            href="https://linkedin.com/in/adham-hatem-94bb99343/"
+            target="_blank"
+          >
+            <img src={linkedinIcon} alt="LinkedIn" className="icon" />
+          </a>
+          <a href="https://github.com/Adham-Hatem" target="_blank">
+            <img src={githubIcon} alt="GitHub" className="icon" />
+          </a>
         </div>
       </div>
     </section>

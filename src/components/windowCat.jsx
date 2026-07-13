d7190cf0fe31136b4262d18382f6d4b7ad5b5abe
Adham Hatem <191885145+Adham-Hatem@users.noreply.github.com>
2026-07-13 13:31:46 +0300

    Polished 'Window Cat' Scaling, Projects Padding & 'Portofolio' Size in Mobile

--- src/components/windowCat.jsx
+++ src/components/windowCat.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { CatTypeContext } from "../context/CatTypeContext";
 import { CloudContext } from "../context/cloudContext";
 import { MoonContext } from "../context/moonContext";
+import cloudIcon from "../assets/cloud-icon.svg";
 import "../catWindow.css";
 import RainLayer from "./rain";
 import WindowDrops from "./rainWindow";
@@ -13,29 +14,66 @@ function WindowCat() {
   const moonCycle = useContext(MoonContext)?.cycle;
 
   return (
-    <section id="window" className="window">
-      <div className="frame">
-        <div className="inner">
-          {/*<StarLayer StarLayer></StarLayer> */}
-          <div className="moon clickable" onClick={moonCycle}>
-            <div className="spots" />
-          </div>
-          <div className="moonCloud clickable" onClick={cloudCycle} />
-          <div className="cloud clickable" onClick={cloudCycle} />
+    <section class="mainContainer">
+      <div id="window" className="window">
+        <div className="frame">
+          <div className="inner">
+            {/*<StarLayer StarLayer></StarLayer> */}
+            <div className="moon" />
+            <div className="moonCloud" />
+            <div className="cloud" />
 
-          <div className="windowDrop" />
-          <div className="glassLayer" />
-          <div className="rainLayer">
-            <RainLayer></RainLayer>
-            <WindowDrops></WindowDrops>
+            <div className="windowDrop" />
+            <div className="glassLayer" />
+            <div className="rainLayer">
+              <RainLayer></RainLayer>
+              <WindowDrops></WindowDrops>
+            </div>
+
+            <div className="catReflection">
+              <div className="cat">
+                <div className="body">
+                  <div className="tail pattern">
+                    <div className="spotBrownTail" />
+                  </div>
+                  <div className="bodyInner pattern">
+                    <div className="spotBrownRight" />
+                    <div className="spotBlackLeft" />
+                  </div>
+                  <div className="head">
+                    <div className="pattern">
+                      <div className="headPattern">
+                        <div className="spotBrown" />
+                        <div className="spotBlack" />
+                      </div>
+                    </div>
+                    <div className="earLeft" />
+                    <div className="earRight" />
+
+                    <div className="eyeLeft" />
+                    <div className="eyeRight" />
+                    <div className="mouth" />
+
+                    <div className="whiskers">
+                      <div className="whiskerTL" />
+                      <div className="whiskerML" />
+                      <div className="whiskerBL" />
+                      <div className="whiskerTR" />
+                      <div className="whiskerMR" />
+                      <div className="whiskerBR" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="catReflection">
+          <div className="rodVertical" />
+          <div className="rodHorizontal" />
+
+          <div className="outer">
             <div className="cat">
               <div className="body">
-                <div className="tail pattern">
-                  <div className="spotBrownTail" />
-                </div>
                 <div className="bodyInner pattern">
                   <div className="spotBrownRight" />
                   <div className="spotBlackLeft" />
@@ -63,51 +101,28 @@ function WindowCat() {
                     <div className="whiskerBR" />
                   </div>
                 </div>
+
+                <div className="tail pattern">
+                  <div className="spotBrownTail" />
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="rodVertical" />
-        <div className="rodHorizontal" />
+      <div className="controlPanel">
+        <button className="windowControlButton clickable" onClick={cycle}>
+          <div className="catColorButton" />
+        </button>
 
-        <div className="outer">
-          <div className="cat clickable" onClick={cycle}>
-            <div className="body">
-              <div className="bodyInner pattern">
-                <div className="spotBrownRight" />
-                <div className="spotBlackLeft" />
-              </div>
-              <div className="head">
-                <div className="pattern">
-                  <div className="headPattern">
-                    <div className="spotBrown" />
-                    <div className="spotBlack" />
-                  </div>
-                </div>
-                <div className="earLeft" />
-                <div className="earRight" />
+        <button className="windowControlButton clickable" onClick={cloudCycle}>
+          <img src={cloudIcon} alt="silhouette" />
+        </button>
 
-                <div className="eyeLeft" />
-                <div className="eyeRight" />
-                <div className="mouth" />
-
-                <div className="whiskers">
-                  <div className="whiskerTL" />
-                  <div className="whiskerML" />
-                  <div className="whiskerBL" />
-                  <div className="whiskerTR" />
-                  <div className="whiskerMR" />
-                  <div className="whiskerBR" />
-                </div>
-              </div>
-
-              <div className="tail pattern">
-                <div className="spotBrownTail" />
-              </div>
-            </div>
-          </div>
-        </div>
+        <button className="windowControlButton clickable" onClick={moonCycle}>
+          <div className="moonIcon" />
+        </button>
       </div>
     </section>
   );

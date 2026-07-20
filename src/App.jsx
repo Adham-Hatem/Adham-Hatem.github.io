1a22682b9429edb02253501518c53004e110db94
Adham Hatem <191885145+Adham-Hatem@users.noreply.github.com>
2026-07-20 07:57:58 +0300

    Testing Scroll Behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import WindowCat from "./components/windowCat";
-import AccentPopEffect from "./hooks/AccentPopEffect";
-import { AccentProvider } from "./context/AccentContext";
-import { CatTypeProvider } from "./context/CatTypeContext";
-import { CloudProvider } from "./context/cloudContext";
-import { MoonProvider } from "./context/moonContext";
-import Background from "./components/Background";
-
 function App() {
   return (
-    <AccentProvider>
-        <div className="pageWrapper">
-          <Background />
-          <AccentPopEffect />
-          <Navbar />
-          <main className="mainContent">
-            <Hero />
-            <About />
-            <Projects />
-            <Contact />
-          </main>
-        </div>
+    <>
+      <div className="pageWrapper">
+        <Background />
+        <AccentPopEffect />
+        <Navbar />
+        <main className="mainContent">
+          <Hero />
+          <About />
+          <Projects />
+          <Contact />
+        </main>
+      </div>
 
-        <Footer />
-    </AccentProvider>
+      <Footer />
+    </>
   );
 }
-
-export default App;

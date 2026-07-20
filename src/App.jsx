c23a73a7b362ab4d1baccb53b138bd602af36867
Adham Hatem <191885145+Adham-Hatem@users.noreply.github.com>
2026-07-20 08:01:07 +0300

    Testing Scroll Behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import WindowCat from "./components/windowCat";
+import AccentPopEffect from "./hooks/AccentPopEffect";
+import { AccentProvider } from "./context/AccentContext";
+import { CatTypeProvider } from "./context/CatTypeContext";
+import { CloudProvider } from "./context/cloudContext";
+import { MoonProvider } from "./context/moonContext";
+import Background from "./components/Background";
+
 function App() {
   return (
-    <>
+    <AccentProvider>
       <div className="pageWrapper">
         <Background />
         <AccentPopEffect />
@@ -14,6 +28,8 @@ function App() {
       </div>
 
       <Footer />
-    </>
+    </AccentProvider>
   );
 }
+
+export default App;
